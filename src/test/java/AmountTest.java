5aa0b4a8050f67121df34cd1999abf0237ece065
Michele Marino <m.marino@reply.it>
2020-08-08 17:50:28 +0200

    dev: tests added

--- src/test/java/AmountTest.java
+++ src/test/java/AmountTest.java
@@ -11,4 +11,26 @@ public class AmountTest {
         final Amount actual = firstAmount.sum(secondAmount);
         Assert.assertEquals(expected, actual);
     }
+
+    @Test
+    public void canCreateAmountFromBaseUnit(){
+        final Amount expected = new Amount(new Penny(6),new Shilling(2),new Pound(9));
+        final Amount actual = Amount.fromBaseUnit(2190);
+        Assert.assertEquals(expected,actual);
+    }
+
+
+    @Test
+    public void canCreatePoundFromBaseUnit(){
+        final Amount expected = new Amount(new Penny(0),new Shilling(0),new Pound(1));
+        final Amount actual = Amount.fromBaseUnit(240);
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void canCreateShillingFromBaseUnit(){
+        final Amount expected = new Amount(new Penny(0),new Shilling(1),new Pound(0));
+        final Amount actual = Amount.fromBaseUnit(12);
+        Assert.assertEquals(expected,actual);
+    }
 }
